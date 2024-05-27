e98b02e46b651101e28b80e7e4afdc0e5d2fa443
Ian Baron <ianezebaron@gmail.com>
2024-05-27 09:03:13 -0300

    Login Manager Script check password (missing button function))

--- Assets/LogInManager.cs
+++ Assets/LogInManager.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogInManager : MonoBehaviour
 {
+
+    public string password = "contraseña";
+    public InputField inputContraseña;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,16 @@ public class LogInManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void CheckPassword()
+    {
+        if (inputContraseña.text == password)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
     }
 }
